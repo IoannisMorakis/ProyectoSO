4e2ee53925bf031376382b1619ee350eec4a24c4
IoannisMorakis <62579052+IoannisMorakis@users.noreply.github.com>
2021-06-27 19:25:50 -0400

    Update Proyecto.c

--- Proyecto.c
+++ Proyecto.c
@@ -7,8 +7,8 @@
 int main(void) {
 	int fd[2];
 	pid_t pid;
-	char string[51];
-	char readbuffer[55]; 
+	char string[21];
+	char readbuffer[25]; 
         char command[50];
 
 	pipe(fd); // crea tuberia
@@ -21,7 +21,7 @@ int main(void) {
 	if(pid) {
 		close(fd[0]);
 		while(1){
-			fgets(string, 50, stdin); // lee comando
+			fgets(string, 21, stdin); // lee comando
 			
 			// funcion que detecta sie es un comado valido aqui
 			
@@ -30,7 +30,7 @@ int main(void) {
 			if(strcmp(string, "salir\n")==0){ // si el comando es "salir" termina proceso padre
 				break;
 			}else{
-				write(fd[1], string, strlen(string)+1); // envia comando a hijo
+				write(fd[1], string, strlen(string)); // envia comando a hijo
 			}
 			
 		}
