bb74a749c7c9a116d7456f1b444f3909ecf2a78c
IoannisMorakis <62579052+IoannisMorakis@users.noreply.github.com>
2021-06-27 14:05:03 -0400

    Update Proyecto.c

--- Proyecto.c
+++ Proyecto.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-/* #include <sys/types.h> */
 
 int main(void) {
 	int fd[2];
@@ -23,17 +22,14 @@ int main(void) {
 		close(fd[0]);
 		while(1){
 			fgets(string, 50, stdin);
-			//scanf("%s", string);
-			//printf("%d\n", strcmp(string, "salir\n"));
+			
 			if(strcmp(string, "salir\n")==0){
-				//printf("salir\n");
 				break;
 			}else{
 				write(fd[1], string, strlen(string)+1);
 			}
 		}
 		kill(pid, SIGKILL);
-		//wait(NULL);
 		
 			
 	} else {
