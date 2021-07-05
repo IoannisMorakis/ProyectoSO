9476a0dfe79df4e7c2ba6b50e2e890d2791760a7
IoannisMorakis <62579052+IoannisMorakis@users.noreply.github.com>
2021-07-04 22:41:45 -0400

    Update Proyecto.c

--- Proyecto.c
+++ Proyecto.c
@@ -5,47 +5,59 @@
 #include <sys/wait.h>
 
 int main(void) {
-	int fd[2];
+	int *fd;
 	pid_t pid;
-	char string[22];
-	char readbuffer[26]; 
+	char string[52];
+	char readbuffer[56]; 
+        char command[50];
 
-	pipe(fd); // crea tuberia
 
-	if((pid = fork()) == -1) {
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
+	while(1){
+		fd=(int*) calloc(2, sizeof(int));
+		pipe(fd); // crea tuberia
+		
+		
+		if((pid = fork()) == -1) {
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
 
-	if(pid) {
-		close(fd[0]);
-		while(1){
-			fgets(string, 21, stdin); // lee comando
+		if(pid) { // Parent
+			close(fd[0]);
 			
+			fgets(string, 51, stdin);
+
 			// funcion que detecta sie es un comado valido aqui
-			
+
 			// si no es un comando valido imprimir "Comando invalido" y seguir al siguiente comando
 			//y si es valido ejecuta esto:
-			if(strcmp(string, "salir\n")==0){ // si el comando es "salir" termina proceso padre
+
+			if(strcmp(string, "salir\n")==0){
+				//printf("salir\n");
+				kill(pid, SIGKILL);
 				break;
 			}else{
-				write(fd[1], string, strlen(string) +1); // envia comando a hijo
+				write(fd[1], string, strlen(string) + 1 );
 			}
 			
-		}
-		kill(pid, SIGKILL); // terminar proceso hijo
-		
-			
-	} else {
-		close(fd[1]);
-		while(1){
-			read(fd[0], readbuffer, sizeof(readbuffer)); // recibe comando
-			system(readbuffer); // ejecuta comando
-		}
-		exit(EXIT_SUCCESS);
-	
-		
-	}
+			//kill(pid, SIGKILL);
+			wait(NULL);
+				
+		} else { // Child
+			close(fd[1]);
+				read(fd[0], readbuffer, sizeof(readbuffer));
+				
+				char *arg[] = {
+					"sh", 
+					"-c",
+					readbuffer, 
+					NULL
+				};
 
+				execvp(arg[0], arg);
+
+		
+		}
+	}
 	exit(EXIT_SUCCESS);
 }
